63b8294738325ba69261dc2e30d6c076abbe5615
Josh Howard <jashghoward@icloud.com>
2023-09-21 21:59:10 +0200

    food_manager.ex update

--- lib/food_manager.ex
+++ lib/food_manager.ex
@@ -10,9 +10,6 @@ defmodule FoodManager do
 
   def get_food_cost(food) do
     case Map.get(@foods, String.downcase(food)) do
-
-
-
       nil -> {:error, "Food not found"}
       cost -> {:ok, cost}
 
