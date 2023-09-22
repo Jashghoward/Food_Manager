99f7cb27ce42a93e14cdf1fabf4359365b9393b7
Josh Howard <jashghoward@icloud.com>
2023-09-22 10:56:19 +0200

    order_manager update

--- lib/order_manager.ex
+++ lib/order_manager.ex
@@ -6,13 +6,20 @@ defmodule OrderManager do
 
 
   def create_order(guest_name, items) do
+
+
     %__MODULE__{guest_name: guest_name, items: items}
   end
 
   def calculate_total(order) do
       Enum.reduce(order.items, 0, fn item, acc ->
+
+
+
       case FoodManager.get_food_cost(item) do
         {:ok, cost} -> acc + cost
+
+
         {:error, _} -> acc
       end
     end)
